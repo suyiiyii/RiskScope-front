d8fd1058ce528af5cd3a57228d7d19566935747d
suyiiyii <suyiiyii@gmail.com>
2024-07-17 19:07:31 +0800

    完成路由架构

--- src/components/layouts/BaseAside.vue
+++ src/components/layouts/BaseAside.vue
@@ -5,30 +5,45 @@
       class="el-menu-vertical-demo"
       style="height: 100%"
     >
-      <el-menu-item index="1">
-        <el-icon>
-          <House />
-        </el-icon>
-        主页
-      </el-menu-item>
-      <el-menu-item index="2">
-        <el-icon>
-          <ZoomIn />
-        </el-icon>
-        风险识别
-      </el-menu-item>
-      <el-menu-item index="3">
-        <el-icon>
-          <View />
-        </el-icon>
-        历史记录
-      </el-menu-item>
+      <RouterLink to="/home">
+
+        <el-menu-item index="1">
+          <el-icon>
+            <House />
+          </el-icon>
+          主页
+        </el-menu-item>
+      </RouterLink>
+      <RouterLink to="scope">
+        <el-menu-item index="2">
+          <el-icon>
+            <ZoomIn />
+          </el-icon>
+          风险识别
+        </el-menu-item>
+      </RouterLink>
+      <RouterLink to="history">
+        <el-menu-item index="3">
+          <el-icon>
+            <View />
+          </el-icon>
+          历史记录
+        </el-menu-item>
+      </RouterLink>
     </el-menu>
 
   </el-aside>
 </template>
 <script>
+import { View } from '@element-plus/icons-vue'
+
 export default {
   name: 'BaseAside'
 }
 </script>
+
+<style scoped>
+a {
+  text-decoration: none;
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -5,18 +5,19 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/home',
       name: 'home',
       component: () => import('../views/HomeView.vue')
     },
     {
       path: '/scope',
       name: 'scope',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/ScopeView.vue')
-    }
+    },{
+      path: '/history',
+      name: 'history',
+      component: () => import('../views/HistoryView.vue')
+    },
   ]
 })
 
