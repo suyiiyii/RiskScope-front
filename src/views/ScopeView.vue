63ef874e9d4afb81770106c3144a74a088996cdb
suyiiyii <suyiiyii@gmail.com>
2024-07-22 15:20:09 +0800

    feat(ScopeView): 根据内容类型分离文本和音频任务提交
    根据所选的contentType，将任务提交路由分别指向'/task_text'和'/task_audio'，
    以处理文本和音频内容的不同提交逻辑。这提高了代码的可读性和可维护性，并为未来可能的
    功能扩展提供了便利。

--- src/views/ScopeView.vue
+++ src/views/ScopeView.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-
-
 import { ref } from 'vue'
 import request from '@/request'
 
@@ -13,23 +11,40 @@ const handleFileChange = (newFile: File) => {
 }
 
 const submit = () => {
-  request().post('/task', {
-    scopeType: 'risk',
-    contentType: contentType.value,
-    textContent: contentType.value == 'text' ? textContent.value : '',
-    audioContent: contentType.value == 'audio' ? audioContent.value.raw : ''
-  }, {
-    headers: {
-      'Content-Type': 'multipart/form-data'
-    }
-  })
+  if (contentType.value == 'text') {
+    request().post(
+      '/task_text',
+      {
+        scopeType: 'risk',
+        contentType: contentType.value,
+        textContent: contentType.value == 'text' ? textContent.value : ''
+      },
+      {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      }
+    )
+  } else {
+    request().post(
+      '/task_audio',
+      {
+        scopeType: 'risk',
+        contentType: contentType.value,
+        audioContent: contentType.value == 'audio' ? audioContent.value.raw : ''
+      },
+      {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      }
+    )
+  }
 }
-
 </script>
 
 <template>
   <div class="contentForm">
-
     <el-form label-width="auto">
       <el-form-item label="内容类型">
         <el-radio-group v-model="contentType">
@@ -39,26 +54,16 @@ const submit = () => {
       </el-form-item>
 
       <el-form-item label="文本识别" v-if="contentType === 'text'">
-        <el-input
-          v-model="textContent"
-          type="textarea"
-          placeholder="请输入文本"
-        ></el-input>
+        <el-input v-model="textContent" type="textarea" placeholder="请输入文本"></el-input>
       </el-form-item>
       <el-form-item label="音频识别" v-if="contentType === 'audio'">
-        <el-upload
-          action="#"
-          :limit="1"
-          :auto-upload="false"
-          :on-change="handleFileChange">
+        <el-upload action="#" :limit="1" :auto-upload="false" :on-change="handleFileChange">
           <template v-slot:trigger>
             <el-button @click="handleFileChange">选取文件</el-button>
           </template>
         </el-upload>
-
       </el-form-item>
-      <div style="display: flex; flex-direction: row-reverse;">
-
+      <div style="display: flex; flex-direction: row-reverse">
         <el-button type="primary" @click="submit">提交</el-button>
       </div>
     </el-form>
@@ -66,7 +71,6 @@ const submit = () => {
 </template>
 
 <style scoped>
-
 .contentForm {
   max-width: 500px;
 }
